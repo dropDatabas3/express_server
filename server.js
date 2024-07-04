3ea18f8642c64b4ebae182c58d4687a92ee6f581
juanp1996 <juanpalcio1996@gmail.com>
2024-07-04 12:36:38 -0300

    Add brotli to compression middleware

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ server.use(express.urlencoded({ extended: true })); // <-- Allows the server to 
 server.use(express.json()); // <-- Used for req body
 server.use(morgan("dev")); // <-- Log requests to the console
 server.use(express.static(__dirname + "/public")); // <-- Serve static files
-server.use(compression()); // <-- Compress the response data
+server.use(compression({brotli: { enabled: true, zlib: {} },}));// <-- Compress the response data
 
 /*************
   ENDPOINTS
